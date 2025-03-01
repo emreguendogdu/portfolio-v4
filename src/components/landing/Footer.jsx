b499b79a4d3e992e58081f2083033ffc4a977b2c
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2025-03-01 22:14:54 +0300

    style: Refactor footer social links to use list items for better semantics

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -5,17 +5,18 @@ export default function Footer() {
   return (
     <footer id="contact" className="flex items-end bg-black text-white text-sm">
       <div className="w-full flex justify-between items-start md:items-end border-t border-t-neutral-800 py-4 px-sectionX-m md:px-sectionX">
-        <ul className="[&_a]:uppercase flex flex-col md:flex-row md:gap-4">
+        <ul className="flex flex-col md:flex-row md:gap-4">
           {socials.map((social, index) => (
-            <a
-              href={social.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="nav-link"
-              key={`fs_${index}`}
-            >
-              {social.name}
-            </a>
+            <li key={`fs_${index}`}>
+              <a
+                href={social.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="nav-link uppercase"
+              >
+                {social.name}
+              </a>
+            </li>
           ))}
         </ul>
         <div className="md:flex md:gap-4 nav-link pointer-events-none">
