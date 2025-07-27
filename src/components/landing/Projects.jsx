5bd1a97b69b2dc3848bc6cc6765a02b8b3b410b0
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2025-07-27 10:08:33 +0300

    fix: Hero is visible before preloader starts; Minor style updates

--- src/components/landing/Projects.jsx
+++ src/components/landing/Projects.jsx
@@ -46,7 +46,7 @@ const Project = ({ project }) => {
       <div className="w-full max-w-3xl aspect-video mx-auto">
         <ProjectImage project={project} />
       </div>
-      <div className="flex flex-col md:items-center justify-center flex-1">
+      <div className="flex flex-col lg:items-center justify-center flex-1">
         <p> {project.name}</p>
         <p className="text-sm font-light uppercase text-neutral-400">
           Design & Development
@@ -60,18 +60,21 @@ export default function Projects() {
   return (
     <section
       id="projects"
-      className="relative bg-linear-to-b to-[#1c1d20] from-[#000] px-sectionX-m md:px-sectionX py-sectionY-m md:pt-sectionY md:pb-xs text-white min-h-screen z-10 -mt-[100dvh] pt-[100dvh]"
+      className="relative bg-linear-to-b to-[#1c1d20] from-[#000] px-sectionX-m lg:px-sectionX py-sectionY-m lg:pt-sectionY lg:pb-xs text-white min-h-screen z-10 -mt-[100dvh] pt-[100dvh]"
     >
-      <div className="flex justify-between">
-        <div className="grid grid-cols-2 gap-x-sm gap-y-md w-1/2">
+      <div className="flex flex-col-reverse lg:flex-row justify-start items-start gap-sm lg:justify-between">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-sm gap-y-md w-full sm:w-3/4 sm:self-center lg:w-1/2">
           {projects.map((project, idx) => (
             <Project key={project.name} project={project} index={idx} />
           ))}
         </div>
         <p className="subheading">(Featured Projects)</p>
       </div>
-      <div className="flex justify-center items-center w-1/2 relative mt-sm">
-        <ShinyButton href="mailto:hello@emregnd.com" className="text-center">
+      <div className="flex justify-center items-center self-center w-full lg:w-1/2 relative mt-sm">
+        <ShinyButton
+          href="mailto:hello@emregnd.com"
+          className="text-center self-center lg:self-auto"
+        >
           Hire Me
         </ShinyButton>
       </div>
--- src/components/ui/Preloader.jsx
+++ src/components/ui/Preloader.jsx
@@ -1,13 +1,12 @@
-import { animate, AnimatePresence, motion } from "motion/react"
+import { animate, motion } from "motion/react"
 import useWindowDimensions from "../../hooks/useWindowDimensions"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 export const PRELOADER_DELAY = 0.25
 export const PRELOADER_DURATION = 1.24
 
 export default function Preloader() {
   const { width } = useWindowDimensions()
-  const [isVisible, setIsVisible] = useState(true)
 
   // Scroll to top on load
   useEffect(() => {
@@ -18,6 +17,9 @@ export default function Preloader() {
   }, [])
 
   const handleAnimate = async () => {
+    animate("body", {
+      overflowY: "hidden",
+    })
     await animate(
       "#preloader-ball",
       {
@@ -41,44 +43,38 @@ export default function Preloader() {
         ease: [0.36, 0, 0.66, -0.25],
       }
     )
+
+    await animate(
+      "#preloader",
+      {
+        opacity: 0,
+        backgroundColor: "transparent",
+        visibility: "hidden",
+      },
+      {
+        duration: 0.25,
+      }
+    )
+
+    await animate("body", {
+      overflowY: "auto",
+    })
   }
 
   useEffect(() => {
     handleAnimate()
   }, [])
 
-  useEffect(() => {
-    if (isVisible) {
-      document.body.style.overflowY = "hidden"
-    } else {
-      setTimeout(() => {
-        document.body.style.overflowY = "auto"
-      }, 500)
-    }
-  }, [isVisible])
-
   return (
-    <AnimatePresence mode="wait">
-      {isVisible && (
-        <motion.div
-          id="preloader"
-          className={`fixed inset-0 h-[100dvh] w-full text-[#000] bg-[#1c1d20] flex items-center justify-center z-50 overflow-hidden`}
-          initial={{ opacity: 1, backgroundColor: "#1c1d20" }}
-          animate={{ opacity: 0, backgroundColor: "transparent" }}
-          transition={{
-            delay: PRELOADER_DURATION + PRELOADER_DELAY,
-            duration: PRELOADER_DELAY,
-            ease: "easeIn",
-          }}
-          onAnimationComplete={() => setIsVisible(false)}
-        >
-          <motion.div
-            className="bg-black rounded-full origin-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16"
-            style={{ scale: 0 }}
-            id="preloader-ball"
-          />
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <motion.div
+      id="preloader"
+      className={`fixed inset-0 h-[100dvh] w-full text-[#000] bg-[#1c1d20] flex items-center justify-center z-50 overflow-hidden`}
+    >
+      <motion.div
+        className="bg-black rounded-full origin-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16"
+        style={{ scale: 0 }}
+        id="preloader-ball"
+      />
+    </motion.div>
   )
 }
